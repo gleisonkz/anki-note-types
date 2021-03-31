b84725d0d0d682063cf9ff43d7ea9d96e1fd21ba
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-03-31 11:52:10 -0300

    📦 refactor: add new typing effect

--- custom-cloze/fields.js
+++ custom-cloze/fields.js
@@ -1,53 +1,53 @@
 const deckName = "🏳️ GENERAL::👨‍💻 DEVELOPMENT::🎨 FRONT-END::🟨 JAVASCRIPT";
 const subDeckName = "JAVASCRIPT";
-const question = "Array.prototype.filter()";
+const question = "Em quantas categorias são divididas os componentes do Angular Material?";
 const answer = "um array vazio.";
 const extra = "Teste de extra";
 const image = "";
 const tip = "";
 
 const replacementElements = [
-    {
-        searchValue: "{{Deck}}",
-        newValue: deckName
-    },
-    {
-        searchValue: "{{Subdeck}}",
-        newValue: subDeckName
-    },
-    {
-        searchValue: "{{Question}}",
-        newValue: question
-    },
-    {
-        searchValue: "{{Answer}}",
-        newValue: answer
-    },
-    {
-        searchValue: "{{Extra}}",
-        newValue: extra
-    },
-    {
-        searchValue: "{{#Extra}}",
-        newValue: ''
-    },
-    {
-        searchValue: "{{/Extra}}",
-        newValue: ''
-    },
-    {
-        searchValue: "{{#Tip}}",
-        newValue: ''
-    },
-    {
-        searchValue: "{{/Tip}}",
-        newValue: ''
-    },
-]
+  {
+    searchValue: "{{Deck}}",
+    newValue: deckName,
+  },
+  {
+    searchValue: "{{Subdeck}}",
+    newValue: subDeckName,
+  },
+  {
+    searchValue: "{{Question}}",
+    newValue: question,
+  },
+  {
+    searchValue: "{{Answer}}",
+    newValue: answer,
+  },
+  {
+    searchValue: "{{Extra}}",
+    newValue: extra,
+  },
+  {
+    searchValue: "{{#Extra}}",
+    newValue: "",
+  },
+  {
+    searchValue: "{{/Extra}}",
+    newValue: "",
+  },
+  {
+    searchValue: "{{#Tip}}",
+    newValue: "",
+  },
+  {
+    searchValue: "{{/Tip}}",
+    newValue: "",
+  },
+];
 
 const replaceFields = (searchValue) => {
-    replacementElements.forEach(c => {
-        searchValue = searchValue.replace(c.searchValue, c.newValue);
-    })
-    return searchValue;
-}
+  replacementElements.forEach((c) => {
+    searchValue = searchValue.replace(c.searchValue, c.newValue);
+  });
+  return searchValue;
+};
